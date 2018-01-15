9f1d738393c77e0bbe18907f37fa48871eb28f3f
Adèle Menanteau <amenanteau@numesia.fr>
2018-01-15 10:23:55 +0100

    Vis added

--- client/build/js/wwae.babel.js
+++ client/build/js/wwae.babel.js
@@ -1699,6 +1699,21 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
   var app = new __WEBPACK_IMPORTED_MODULE_6__objects_Application_js__["a" /* default */](informationsPanel, strategiePanel, attributsPanel, populationPanel);
 
+  // create an array with nodes
+  var nodes = new vis.DataSet([{ id: 1, label: 'Node 1' }, { id: 2, label: 'Node 2' }, { id: 3, label: 'Node 3' }, { id: 4, label: 'Node 4' }, { id: 5, label: 'Node 5' }]);
+
+  // create an array with edges
+  var edges = new vis.DataSet([{ from: 1, to: 3 }, { from: 1, to: 2 }, { from: 2, to: 4 }, { from: 2, to: 5 }, { from: 3, to: 3 }]);
+
+  // create a network
+  var container = document.getElementById('mynetwork');
+  var data = {
+    nodes: nodes,
+    edges: edges
+  };
+  var options = {};
+  var network = new vis.Network(container, data, options);
+
   /***/
 },
 /* 2 */
