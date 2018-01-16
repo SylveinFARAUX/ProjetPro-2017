5dcbcde201decc9b0f359f0fb16f63bf808a13df
fbrossar25 <florian.brossard25@gmail.com>
2018-01-16 21:05:53 +0100

    problème hauteur résolu

--- client/js/Main.js
+++ client/js/Main.js
@@ -54,6 +54,7 @@ network.on("click", function (params) {
     params.event = "[original event]";
     document.getElementById('eventSpan').innerHTML = '<h2>Click event:</h2>' + JSON.stringify(params, null, 4);
     console.log('click event, getNodeAt returns: ' + this.getNodeAt(params.pointer.DOM));
+    console.log('plop');
 });
 network.on("doubleClick", function (params) {
     params.event = "[original event]";
