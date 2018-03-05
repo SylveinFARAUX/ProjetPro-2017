3da632b6fa04e70c34e8ec6b07bc51b9a201fa70
Unknown <clovismaillard@gmail.com>
2018-03-05 14:40:57 +0100

    Ajout division Population - unitaire
    Ajout, de la division Population, dans un document séparé  "population.html", avec tests unitaires

--- client/js/objects/Character.js
+++ client/js/objects/Character.js
@@ -1,33 +1,50 @@
 class Character{
-    constructor(nom, attributs, img){
+    constructor(nom, attributs, img, id){
         this.nom = nom;
         this.attributs = attributs;
         this.img = img;
+		this.active = true;
+		this.id = id;
     }
+	
+	constructor(json, id){
+		this.id = id;
+        this.nom = json.nom;
+        this.img = json.img;
+		this.active = true;
+		this.attributs = new Array();
+		for(var i = 0; i < json.attributs.length; i++){
+			this.attributs.push(getAttributeInstance(json.attributs[i].key, json.attributs[i].value));
+		}
+    }
+	
     
     getImg(){
         return this.img;
-    }
-    
-    setImg(img){
-        this.img = img;
     }
     
     getNom(){
         return this.nom;
     }
     
-    setNom(nom){
-        this.nom = nom;
-    }
-    
     getAttributs(){
         return this.attributs;
     }
-    
-    setAttributs(attributs){
-        this.attributs = attributs;
-    }
+
+	active(){
+		if(this.active)return;
+		Document.getElementById("charimg" + this.id).style.filter = "";
+		Document.getElementById("charstatus" + this.id).innerHTML = "Suspect";
+		this.active = true;
+	}
+	
+	unactive(reason){
+		if(!this.active)return;
+		Document.getElementById("charimg" + this.id).style.filter = "grayscale(100%)";
+		Document.getElementById("charstatus" + this.id).innerHTML = "Eliminé : " + reason;
+		this.active = false;
+	}
+
 }
 
 export default Character;
