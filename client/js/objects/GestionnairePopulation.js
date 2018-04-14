4cd19b4453edb9af7e007e73cca0c67f680bf055
fbrossar25 <florian.brossard25@gmail.com>
2018-04-14 14:47:03 +0200

    bourdasse

--- client/js/objects/GestionnairePopulation.js
+++ client/js/objects/GestionnairePopulation.js
@@ -330,7 +330,7 @@ class GestionnairePopulation {
         let attrKeys = document.getElementById("tooltip" + idChar).getElementsByClassName("attr_id");
         let attrVals = document.getElementById("tooltip" + idChar).getElementsByClassName("attr_value");
         for(let i = 0; i < attrKeys.length; i++){
-            attr = new Object()
+            attr = {};
             attr.key = attrKeys[i].innerHTML;
             attr.value = attrVals[i].innerHTML;
             char.attributs.push(attr);
